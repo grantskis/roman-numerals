750805c71040821dc3d64c27f6cbd7504e1046d4
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-29 14:55:00 -0700

    move test script snippets to new separate test file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,4 @@
 function usableNumber(number) {
-  if (number < 4000) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-function usableNumber2(number) {
-  if (number > 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-function usableNumber3(number) {
-  if (number % 1 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-
-function usableNumber(number) {
   if (number >= 4000) {
     return false;
   } else if (number < 0) {
@@ -34,4 +9,4 @@ function usableNumber(number) {
     return true;
   }
 };
-usableNumber();
+
