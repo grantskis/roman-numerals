670f3fc805036b0a5a3fa883b46e3d2a234f3bc9
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-29 13:15:50 -0700

    pass all usableNumber tests.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,33 @@ function usableNumber(number) {
     return false;
   }
 };
+
+function usableNumber2(number) {
+  if (number > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function usableNumber3(number) {
+  if (number % 1 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+function usableNumber(number) {
+  if (number >= 4000) {
+    return false;
+  } else if (number < 0) {
+    return false;
+  } else if (number % 1 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+usableNumber();
