02b72b9ee500198b00603b2dbdeb3983abaae7d8
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-29 21:08:41 -0700

    supply all working business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
+// BUSINESS LOGIC........ LMAO
 let remainder = [];
 let chainArray = [];
+let finalChain = "";
 function addM(number) {
   let outputThousandsPlace = [];
   while 
@@ -49,34 +51,55 @@ function allAdds(number) {
   addC(remainder);
   addX(remainder);
   addI(remainder);
-  console.log(chainArray);
-};
 
-chainArray.forEach(function(variable) {
-  let c1 = variable.charAt(0);
-  let c2 = variable.charAt(1);
-  let c3 = variable.charAt(2);
-  let c4 = variable.charAt(3);
-  let c5 = variable.charAt(4);
-  if ( c1 === c2 &&  c3 === c4 && c2 === c5 && c3 === c5 ) {
-    if ( c1 === "I") {
-      variable = variable.replace("IIIII", "V");
-    } else if ( c1 === "X") {
-      variable = variable.replace("XXXXX", "L");
-    } else if ( c1 === "C") {
-      variable = variable.replace("CCCCC", "D");
-    } else {
-        console.log("How did you even get this error?")
+  chainArray.forEach(function(variable) {
+    let c1 = variable.charAt(0);
+    let c2 = variable.charAt(1);
+    let c3 = variable.charAt(2);
+    let c4 = variable.charAt(3);
+    let c5 = variable.charAt(4);
+    if ( c1 === c2 &&  c3 === c4 && c2 === c5 && c3 === c5 ) {
+      if ( c1 === "I") {
+        if (variable === "IIIIIIIII") {
+          variable = "IX"
+        }
+        else if (variable === "IIII") {
+          variable = "IV"
+        }
+        else {
+          variable = variable.replace(/I{5}/, "V");
+        }
+        chainArray[3] = variable;
+      } else if ( c1 === "X") {
+        if (variable === "XXXXXXXXX") {
+          variable = "XC";
+        } else if (variable === "XXXX") {
+          variable = "XL";
+        } else {
+          variable = variable.replace(/X{5}/, "L");
       }
-  } else {
-  return false; 
-  };
-  console.log(variable);
-  console.log(chainArray);
+        chainArray[2] = variable;
+      } else if ( c1 === "C") {
+        if (variable === "CCCCCCCCC") {
+          variable = "CM";
+        } else if (variable === "CCCC") {
+          variable = "CD";
+        } else {
+          variable = variable.replace("CCCCC", "D");
+        };
+        chainArray[1] = variable;
+      } else {
+          console.log("How did you even get this error?")
+        }
+    } else {
+    return false; 
+    };
+    });
+  chainArray.forEach(function(linkOfChain) {
+    finalChain = finalChain.concat(linkOfChain);
   });
-
-
-
+  console.log(finalChain)
+  };
 
 function usableNumber(number) {
   if (number >= 4000) {
@@ -89,5 +112,22 @@ function usableNumber(number) {
     allAdds(number);
   }
 };
+// 
+
+// USER LOGIC... LMAO
+$(document).ready(function() {
+  $(#number-box).submit(function(event) {
+    event.preventDefault();
+    const arabicNumerals = $("input#numbers").val();
+    usableNumber(arabicNumerals);
+    $(".romanNumerals").text(finalChain);
+    $(".output").show();
+  });
+});
+
+
+
+
 
 usableNumber()
+
