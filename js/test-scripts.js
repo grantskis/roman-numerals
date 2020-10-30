1d25dfecdad7a4342436816f0e458ffc322f66ca
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-29 20:18:37 -0700

    add tests to replace character strings (with many messes of trial and error.)

--- js/test-scripts.js
+++ js/test-scripts.js
@@ -45,7 +45,7 @@
       };
 
 // TEST THE REPLACEMENT OF INTEGERS // 
-    function redactedCode() {
+    function savedCode() {
               let outputThousandsPlace = [];
               function addM(number) {
                 while 
@@ -81,10 +81,6 @@
     };
 // 
 
-
-console.log(outputHundredsPlace);
-let output = outputHundredsPlace.join("");
-console.log(output);
 
 let outputThousandsPlace = [];
 let outputHundredsPlace = [];
@@ -217,4 +213,245 @@ function fivePlus(element) {
   };
 };
 
+
+let godIDK = false;
+const testArray = ["MMMMM", "CCC", "XX", "IIIII"];
+testArray.forEach
+
+function tester(input) {
+  const allCheck = /I|X|C|M{5}/i;
+  if (allCheck.test(input.substring(0))) {
+    if (/I{5}/i.test(input.substring(0))) {
+    console.log("5 I's");
+    godIDK = true;
+    } else if (/X{5}/i.test(input.substring(0))) {
+      console.log("5 X's");
+      godIDK = true;
+    } else if (/C{5}/i.test(input.substring(0))) {
+      console.log("5 C's");
+      godIDK = true;
+    } else if (/M{5}/i.test(input.substring(0))) {
+      console.log("5 M's");
+      godIDK = true;
+    };
+  } else {
+    return false;
+  };
+};
+
+// check if first five letters are the same
+function tester(variable) {
+  let c1 = variable.charAt(0);
+  let c2 = variable.charAt(1);
+  let c3 = variable.charAt(2);
+  let c4 = variable.charAt(3);
+  let c5 = variable.charAt(4);
+  if ( c1 === c2 &&  c3 === c4 && c2 === c5 && c3 === c5 ) {
+  return true;
+  } else {
+  return false; 
+  };
+  };
+  tester("aaaaa")
+
+  const check = /I{5}/i;
+  if (check.test(element.substring(0))) {
+    newElement = element.replace(check, "V")
+
+function oneToV(element) {
+  const iCheck = /I{5}/i;
+  element.replace(check, "V");
+}
+
+
+// CHANGE THE VARIABLES!!!
+function fivePlus(element, change) {
+  const check = /.{5}/i;
+  if (check.test(element.substring(0))) {
+    element = element.replace(check, change)
+  } else {
+    return false;
+  };
+};
+fivePlus("")
+
+function findReplace(c1) {
+if (c1 === "/i/i") {
+  fivePlus("I", "V");
+} else if (c1 === "/x/i") {
+  fivePlus("X", "L");
+}  else if (c1 === "/c/i") {
+  fivePlus("C", "D");
+} else {
+  return false;
+};
+}
+
+
+
+
+
+function tenToL()
+
+function hundredToD()
+
+
+
+
+
+tester("")
+
+
+const iCheck = /I{5}/i;
+const xCheck = /X{5}/i;
+const cCheck = /C{5}/i;
+const mCheck = /M{5}/i;
+
+
+
+
+
+
 fivePlus("IIIIIIII")
+
+function findReplace(element, fives, ones) {
+  const checkFive = /fives{5}/i;
+
+
+
+
+  console.log(checkFive);
+  console.log(element, fives, ones);
+};
+
+
+
+//   if (checkFive.test(element.substring(0))) {
+//     replaceOne = element.replace(checkFive, ones)
+//     console.log(findReplace);
+//   } else {
+//     return false;
+//   };
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function fivePlus(element, change) {
+  const check = /.{5}/i;
+  if (check.test(element.substring(0))) {
+    element = element.replace(check, change)
+  } else {
+    return false;
+  };
+};
+
+
+function tester(variable) {
+  let c1 = variable.charAt(0);
+  let c2 = variable.charAt(1);
+  let c3 = variable.charAt(2);
+  let c4 = variable.charAt(3);
+  let c5 = variable.charAt(4);
+  if ( c1 === c2 &&  c3 === c4 && c2 === c5 && c3 === c5 ) {
+    if ( c1 === "I") {
+      variable = variable.replace("IIIII", "V");
+    } else if ( c1 === "X") {
+      variable = variable.replace("XXXXX", "L");
+    } else if ( c1 === "C") {
+      variable = variable.replace("CCCCC", "D");
+    } else {
+        console.log("How did you even get this error?")
+      }
+  } else {
+  return false; 
+  };
+  console.log(variable)
+  };
+  tester("aaaaa")
+  
+
+  function findReplace(c1) {
+    if (c1 === "/i/i") {
+      fivePlus("I", "V");
+    } else if (c1 === "/x/i") {
+      fivePlus("X", "L");
+    } else if (c1 === "/c/i") {
+      fivePlus("C", "D");
+    } else {
+      return false;
+    };
+    }
+    
+    function fivePlus(element, change) {
+      const check = /.{5}/i;
+      if (check.test(element.substring(0))) {
+        element = element.replace(check, change)
+      } else {
+        return false;
+      };
+    };
+
+
+
+    ////////////////////
+    let chainArray = ["MMM", "CCCCC", "XXXXXXXX", "IIIIIIIII"]
+chainArray.forEach(function(variable) {
+  let c1 = variable.charAt(0);
+  let c2 = variable.charAt(1);
+  let c3 = variable.charAt(2);
+  let c4 = variable.charAt(3);
+  let c5 = variable.charAt(4);
+  if ( c1 === c2 &&  c3 === c4 && c2 === c5 && c3 === c5 ) {
+    if ( c1 === "I") {
+      variable = variable.replace("IIIII", "V");
+      chainArray[3] = variable
+    } else if ( c1 === "X") {
+      variable = variable.replace("XXXXX", "L");
+      chainArray[2] = variable
+    } else if ( c1 === "C") {
+      variable = variable.replace("CCCCC", "D");
+      chainArray[1] = variable
+    } else {
+        console.log("How did you even get this error?")
+      }
+  } else {
+  return false; 
+  };
+  console.log(variable);
+  console.log(chainArray);
+  });
+
+/////////////
